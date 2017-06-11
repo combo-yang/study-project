1a4887cba629d6c2c5e1373d3afc45fe56f8ec01
YZ <1023484292@qq.com>
2017-06-11 17:32:31 +0800

    add：练习表达式和变量

---
+++ FirstDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            const int killone = 5;
+            const int killtwo = ++killone;
+            const int killx = --killone;
+                killx += killtwo;
+            Console.WriteLine(killone);
+            Console.WriteLine(killx);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
